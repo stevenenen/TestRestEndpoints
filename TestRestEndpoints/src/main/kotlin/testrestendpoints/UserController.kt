a4638b3613cd8a552fd8f87e90723d07c11791ea
Steven Kok <skok@cfp.nl>
2022-08-16 10:14:15 +0200

    clean up

--- TestRestEndpoints/src/main/kotlin/testrestendpoints/UserController.kt
+++ TestRestEndpoints/src/main/kotlin/testrestendpoints/UserController.kt
@@ -1,26 +1,87 @@
 package testrestendpoints
 
-import org.springframework.web.bind.annotation.*
-import java.util.*
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.SerializerProvider
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer
+import com.fasterxml.jackson.databind.ser.std.StdSerializer
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDate
 
-@RestController()
-class UserController {
+data class PostUserRequest(
+    val username: String,
+    @JsonProperty(required = false)
+    val age: Int
+)
 
-    data class PostUserRequest(
-        val username: String
-    )
+data class PostUserResponse(
+    val username: String,
+    val age: Int,
+    val created: LocalDate,
+    @JsonSerialize(using = UserTypeSerializer::class)
+    @JsonDeserialize(using = UserTypeDeserializer::class)
+    val type: Type
+)
 
-    @PostMapping("/users")
-    fun postUser(@RequestBody request: PostUserRequest) {
-    }
-
-    data class GetUserResponse(
-        val id: UUID,
-        val username: String
-    )
-
-    @GetMapping("/users/{username}")
-    fun getUser(@PathVariable("username") username: String): GetUserResponse {
-        return GetUserResponse(UUID.randomUUID(), username)
+class UserTypeSerializer : StdSerializer<Type>(Type::class.java) {
+    override fun serialize(value: Type, gen: JsonGenerator, provider: SerializerProvider) {
+        with(gen) {
+            writeStartObject()
+            // upper -> uppercase
+            writeStringField("uppercase", value.name.uppercase())
+            // lower -> lowercase
+            writeStringField("lowercase", value.name.lowercase())
+            writeEndObject()
+        }
     }
 }
+
+class UserTypeDeserializer : StdDeserializer<Type>(Type::class.java) {
+    override fun deserialize(p: JsonParser, ctxt: DeserializationContext): Type {
+        val node = p.readValueAsTree<JsonNode>()
+        // upper -> uppercase
+        val value = node.get("uppercase").textValue()
+        return Type.valueOf(value)
+    }
+}
+
+@RestController
+class UserController {
+    @PostMapping("/users")
+    fun postUser(@RequestBody request: PostUserRequest): PostUserResponse {
+        return PostUserResponse(
+            username = request.username,
+            age = request.age,
+            created = LocalDate.of(2022, 1, 1),
+            Type.ADMIN
+        )
+    }
+}
+
+enum class Type {
+    ADMIN,
+    POWER_USER
+}
+
+//
+//    @PostMapping("/users")
+//    fun postUser(@RequestBody request: PostUserRequest) {
+//    }
+//
+//    data class GetUserResponse(
+//        val id: UUID,
+//        val username: String
+//    )
+//
+//    @GetMapping("/users/{username}")
+//    fun getUser(@PathVariable("username") username: String): GetUserResponse {
+//        return GetUserResponse(UUID.randomUUID(), username)
+//    }
+//}
--- TestRestEndpoints/src/test/kotlin/testrestendpoints/TestRestEndpoints.kt
+++ TestRestEndpoints/src/test/kotlin/testrestendpoints/TestRestEndpoints.kt
@@ -1,25 +1,27 @@
 package testrestendpoints
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.assertj.core.api.Assertions.assertThat
+import org.hamcrest.core.IsEqual
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.get
+import org.springframework.test.web.servlet.post
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
-import testrestendpoints.UserController.GetUserResponse
+import java.time.LocalDate
 
 @WebMvcTest(UserController::class)
 class TestRestEndpoints {
 
     @Autowired
     private lateinit var mvc: MockMvc
-
     private lateinit var objectMapper: ObjectMapper
 
     @BeforeEach
@@ -28,58 +30,60 @@ class TestRestEndpoints {
             .Builder()
             .build()
 
-        objectMapper = ObjectMapper().registerModule(kotlinModule)
+        objectMapper = ObjectMapper()
+            .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, true)
+            .registerModule(kotlinModule)
+            .registerModule(JavaTimeModule())
     }
 
     @Test
     fun `with object mapper`() {
         val username = "Steven"
+        val age = 1
+
+        // Object -> JSON String
+        val request = PostUserRequest(username = username, age)
+        val json = objectMapper.writeValueAsString(request)
 
         // POST /users
-        val contentAsString = objectMapper.writeValueAsString(UserController.PostUserRequest("Steven"))
-
-        mvc.perform(
+        val response = mvc.perform(
             post("/users")
+                .content(json)
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(contentAsString)
         ).andExpect(status().isOk)
+            .andReturn().response.contentAsString
 
-        // GET /users
-        val response = mvc.get("/users/$username")
-            .andExpect {
-                status { isOk() }
-            }.andReturn().response.contentAsString
+        // JSON String -> Object
+        val actual = objectMapper.readValue(
+            response,
+            PostUserResponse::class.java
+        )
 
         // Verify
-        val responseObject = objectMapper.readValue(response, GetUserResponse::class.java)
-        assertThat(responseObject.username).isEqualTo(username)
+        val expected = PostUserResponse(username, age, LocalDate.of(2022, 1, 1), Type.ADMIN)
+        assertThat(actual).isEqualTo(expected)
     }
 
     @Test
     fun `with string literal`() {
         val username = "Steven"
+        val body = """
+            {
+                "username": "$username"
+            }
+        """.trimIndent()
 
         // POST /users
-        mvc.perform(
-            post("/users")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(
-                    """
-                    {
-                        "username": "$username"
-                    }
-                """.trimIndent()
-                )
-        ).andExpect(status().isOk())
-
-        // GET /users
-        mvc.get("/users/$username")
-            .andExpect {
-                status { isOk() }
-                content {
-                    jsonPath("$.id") { exists() }
-                    jsonPath("$.username") { value(username) }
-                }
+        mvc.post("/users") {
+            contentType = MediaType.APPLICATION_JSON
+            content = body
+        }.andExpect {
+            status { isOk() }
+            content {
+                jsonPath("$.username") { value(username) }
+                jsonPath("$.type.upper", IsEqual("ADMIN"))
+                jsonPath("$.type.lower", IsEqual("admin"))
             }
+        }
     }
 }
